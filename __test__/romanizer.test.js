a3ed53776d1afbaba927f0e3cecd86b974816be0
yurong <rong.yu@thoughtworks.com>
2020-08-10 15:23:42 +0800

    [yurong] modify the test

--- __test__/romanizer.test.js
+++ __test__/romanizer.test.js
@@ -1,21 +1,9 @@
-// import {transform} from '../src/romanizer'
+import {transform} from '../src/romanizer'
 
-// describe('transform', () => {
-//     it('should return X when given 10', () => {
-//         const result = transform(10);
-//         expect(result).toEqual("X");
-//     });
-
-// });
-
-
-import { transform } from '../src/romanizer.js'
-
-describe('calculator', () => {
-    fit('should return X when transform 10', () => {
-        const formatted = transform(10);
-
-        expect(formatted).toEqual('X');
+describe('transform', () => {
+    it('should return X when given 10', () => {
+        const result = transform(10);
+        expect(result).toEqual("X");
     });
 
 });
